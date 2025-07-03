31b0d65c5f0f3a1554b952a1c68befa520160de1
gileaddias <gfarias.gpf@gmail.com>
2025-07-03 15:11:38 -0300

    Criando botoes e adicionando funçoes

--- cadastro_estudante/main.py
+++ cadastro_estudante/main.py
@@ -58,12 +58,99 @@ caminho_logo = os.path.join(diretorio_atual, 'logo.png')
 
 # Abrir a imagem com caminho seguro
 app_lg = Image.open(caminho_logo)
-
-#app_lg = Image.open('../logo.png')
 app_lg = app_lg.resize((50,50))
 app_lg = ImageTk.PhotoImage(app_lg)
-app_logo = Label(frame_logo, image=app_lg, text="Cadastro de Alunos", width=850, compound=LEFT, relief=RAISED, anchor=NW, font=('Ivy 15 bold'), bg=co6, fg=co1)
+app_logo = Label(frame_logo, image=app_lg, text="  Cadastro de Alunos", width=850, compound=LEFT, relief=RAISED, anchor=NW, font=('Ivy 15 bold'), bg=co6, fg=co1)
 app_logo.place(x=0, y=0)
+
+# funcção para cadastra alunos
+def alunos():
+    print('Aluno')
+
+
+
+# Função para adicionar cursos e turmas
+def adicionar():
+    print('Cursos e turmas')
+
+
+# Função para salvar
+def salvar():
+    print('Salvar')
+
+
+# Função de Control------------------------------------------------------------------
+
+def control(i):
+    # cadastro de aluno
+    if i == 'cadastro':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função alunos
+        alunos()
+
+    if i == 'adicionar':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função adicionar
+        adicionar()
+
+    if i == 'salvar':
+        for widget in frame_detalhes.winfo_children():
+            widget.destroy()
+
+        for widget in frame_tabela.winfo_children():
+            widget.destroy()
+
+        # chamando a função salvar
+        salvar() 
+
+
+
+
+
+# Criando botoes
+    # Caminho absoluto para o arquivo add.png
+diretorio_atual = os.path.dirname(__file__)
+caminho_app_img_cadastro = os.path.join(diretorio_atual, 'add.png')
+
+app_img_cadastro = Image.open(caminho_app_img_cadastro)
+app_img_cadastro = app_img_cadastro.resize((18,18))
+app_img_cadastro = ImageTk.PhotoImage(app_img_cadastro)
+app_cadastro = Button(frame_dados, command=lambda:control('cadastro'), image=app_img_cadastro, text=" Cadastro", width=100, compound=LEFT, overrelief=RIDGE, font=('Ivy 11'), bg=co1, fg=co0)
+app_cadastro.place(x=10, y=30)
+
+    # Caminho absoluto para o arquivo add.png
+diretorio_atual = os.path.dirname(__file__)
+caminho_app_img_adicionar = os.path.join(diretorio_atual, 'add.png')
+
+app_img_adicionar = Image.open(caminho_app_img_adicionar)
+app_img_adicionar = app_img_adicionar.resize((18,18))
+app_img_adicionar = ImageTk.PhotoImage(app_img_adicionar)
+app_adicionar = Button(frame_dados, command=lambda:control('adicionar'), image=app_img_adicionar, text=" Adicionar", width=100, compound=LEFT, overrelief=RIDGE, font=('Ivy 11'), bg=co1, fg=co0)
+app_adicionar.place(x=123, y=30)
+
+    # Caminho absoluto para o arquivo add.png
+diretorio_atual = os.path.dirname(__file__)
+caminho_app_img_salvar = os.path.join(diretorio_atual, 'save.png')
+
+app_img_salvar = Image.open(caminho_app_img_salvar)
+app_img_salvar = app_img_salvar.resize((18,18))
+app_img_salvar = ImageTk.PhotoImage(app_img_salvar)
+app_salvar = Button(frame_dados, command=lambda:control('salvar'), image=app_img_salvar, text=" Salvar", width=100, compound=LEFT, overrelief=RIDGE, font=('Ivy 11'), bg=co1, fg=co0)
+app_salvar.place(x=236, y=30)
+
+
+
+
 
 
 
